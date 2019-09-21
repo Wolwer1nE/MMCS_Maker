cc6e8016ec0adfb48a4cefaa3e82082ff3ec77d1
wolwer1ne <wolwerine@yandex.ru>
2019-09-21 09:58:52 +0300

    temp fix for beacon import

--- js/scene-map.js
+++ js/scene-map.js
@@ -2,7 +2,39 @@
 *
 */
 import Level from "./level.js"
-import AddBeacon from "./utils/beacon.js"
+function AddBeacon(object)
+{
+  Object.assign(object,
+      {
+        __beacon : new Phaser.Events.EventEmitter(),
+
+        emit(event, payload)
+        {
+          this.__beacon.emit(event, payload);
+        },
+        on(event, callback, context)
+        {
+          this.__beacon.on(event, callback, context);
+        },
+        off(event, callback, context)
+        {
+          this.__beacon.off(event, callback, context);
+        },
+        once(event, callback, context)
+        {
+          this.__beacon.once(event, callback, context);
+        },
+        removeListener(event, callback, context, once)
+        {
+          this.__beacon.removeListener(event, callback, context, once);
+        },
+        removeAllListeners(event)
+        {
+          this.__beacon.removeAllListeners(event);
+        }
+      }
+  );
+}
 
 export default class SceneMap extends Phaser.Tilemaps.Tilemap
 {
